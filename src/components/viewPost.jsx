d948e8434c48b37b2d8c0c1aed74e6022ca771c1
Salem Ali <salemali0705@gmail.com>
2026-05-20 20:10:25 +0530

    feat : view post

--- src/components/viewPost.jsx
+++ src/components/viewPost.jsx
@@ -1,17 +1,37 @@
 import { useParams } from "react-router-dom";
 import appwriteService from "../appwrite/config";
+import { useEffect, useState } from "react";
 
 function ViewPost() {
 
+  const [data,setData] = useState();
   const {slug} = useParams();
-  const post = ()=>{
-    appwriteService.getPost(slug)
+  useEffect(()=>{
+    const targetPost = async ()=>{
+      const fetchedPost = await appwriteService.getPost(slug);
+  
+      if (fetchedPost) {
+        setData(fetchedPost);
+      }
+    }
+    targetPost();
+    
+  },[slug])
+
+  if (!data) {
+      return <div>Loading...</div>
+  }else{
+    return (
+      <div>
+        <img 
+          src={appwriteService.getFilePreview(data.featuredImage)}
+        />
+        <p>{data.$createdAt}</p> 
+        <h1>{data.title}</h1>
+        <p>{data.content}</p>
+      </div>
+    );
   }
-  return (
-    <div>
-      
-    </div>
-  );
 }
 
 export default ViewPost;
